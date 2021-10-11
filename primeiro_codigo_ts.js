8d25a1cd5216fc6069e3385c95d77ef081d9bc2e
eltonpering <pering@edu.univali.br>
2021-10-11 12:57:57 -0300

    commit

--- primeiro_codigo_ts.js
+++ primeiro_codigo_ts.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+console.log("Hello World! Este é meu primeiro código TypeScript");
+console.log("=================");
+var nome = "João";
+console.log("Olá " + nome + ". Seja bem-vindo!");
+console.log("=================");
+var Produto = /** @class */ (function () {
+    function Produto(produtoNome, produtoValor) {
+        this.nome = produtoNome;
+        this.valor = produtoValor;
+    }
+    return Produto;
+}());
+var playstation5 = new Produto("Playstation 5", 5000);
+console.log("=================");
+var elemento = document.querySelector('div');
