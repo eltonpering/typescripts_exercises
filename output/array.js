81e607249d5436198e4d42d46d191a5750c71783
eltonpering <pering@edu.univali.br>
2021-10-12 19:50:05 -0300

    commit

---
+++ output/array.js
@@ -0,0 +1,10 @@
+"use strict";
+const filmesCategorias = ["Comédia", "Drama", "Romance", "Aventura"];
+// filmesCategorias.push("Rom");
+// filmesCategorias.push(123456);
+const filmesAnos = [];
+for (let ano = 2000; ano <= new Date().getFullYear(); ano++) {
+    filmesAnos.push(ano);
+}
+console.log(filmesAnos);
+//# sourceMappingURL=array.js.map
